6e4b3e5179a1300f7e6b1cea6180407bba8866f2
Lorena <lorena.mattos6@gmail.com>
2023-11-16 16:51:36 -0300

    Add email input field and adjust logo position in login view

--- lib/View/loginview.dart
+++ lib/View/loginview.dart
@@ -20,9 +20,19 @@ class _LoginViewState extends State<LoginView> {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset('assets/images/logo.png'),
-          ],
-        ),
+          Image.asset("assets/images/logo.png"),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 0, left: 32, top: 0, right: 32),
+          child:
+            TextField(
+              decoration: InputDecoration(
+                icon: ImageIcon(AssetImage('assets/icons/envelope.png')),
+                hintText: 'E-mail'
+                ),
+            ), // Added closing parenthesis here
+          ),
+        ],
+      ),
       ),
     );
   }
